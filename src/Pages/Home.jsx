e2f705494e0e95e593562c7fef7be96cbac40233
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-11 14:43:48 +0530

    added form

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <form>
+        <label>Name</label>
+        <br />
+        <input type="text" />
+        <br />
+        <label>Contact</label>
+        <br />
+        <input type="number" />
+        <br />
+        <label>Age</label>
+        <br />
+        <input type="number" />
+        <br />
+        <label>Email</label>
+        <br />
+        <input type="text" />
+        <br />
+        <button>Add</button>
+      </form>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
