2374ca09995139f08e5c92a03e2fbb5a53fa7b02
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-12 01:39:43 +0530

    Redux

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { setUser } from "../slices/userSlice";
 
 function Home() {
+  const dispatch = useDispatch();
   const [inputForm, setFormInput] = useState({
     name: "",
     age: "",
@@ -12,13 +15,14 @@ function Home() {
     const { name, value } = event.target;
 
     setFormInput((currValue) => {
-      return { ...currValue, 
-        [name]: value,
-      
-      };
+      return { ...currValue, [name]: value };
     });
   };
 
+  const AddUser = (event) => {
+    event.preventDefault(); 
+    dispatch(setUser(inputForm));
+  };
 
   console.log(inputForm);
 
@@ -63,7 +67,7 @@ function Home() {
           onChange={handleState}
         />
         <br />
-        <button>Add</button>
+        <button onClick={AddUser}>Add</button>
       </form>
     </div>
   );
--- src/Pages/User.jsx
+++ src/Pages/User.jsx
@@ -2,10 +2,22 @@ import React from "react";
 import { useSelector } from "react-redux";
 
 function Users() {
-
   const users = useSelector((state) => state.usersInfo.users);
-console.log(users);
-  return <div>user</div>;
-} 
+  console.log(users);
+  return (
+    <div>
+      {users?.map((user, index) => {
+        return (
+          <div key={index}>
+            <h1>{user.name}</h1>
+            <h1>{user.age}</h1>
+            <h1>{user.email}</h1>
+            <h1>{user.contact}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
 
 export default Users;
