8a720a9f57eec8c440640d38240868925c4b3ab8
Dev_Ak <aakash.velu.003@gmail.com>
2025-11-11 19:33:33 +0530

    initial store

---
+++ src/app/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from "@reduxjs/toolkit";
+import userReducer from "../slices/userSlice";
+
+
+
+
+const store = configureStore({
+    reducer: {
+        userInfo: userReducer,
+    },
+})
+
+
+export default store;
